a9522f403b3c1f3e48fa85307841d8e5b5802c4c
Kwon Valerii <wellko24@gmail.com>
2022-12-02 10:33:24 +0600

    added types and combine all components in container

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,18 @@ import React from 'react';
 import NavBar from "./NavBar/NavBar";
 import {Route, Routes} from "react-router-dom";
 import 'bootstrap/dist/css/bootstrap.min.css';
+import PostForm from "./PostForm/PostForm";
+import HomePage from "./HomePage/HomePage";
+import PostPage from "./PostPage/PostPage";
 
 function App() {
   return (
     <div className="App">
       <NavBar/>
         <Routes>
-            <Route path='/' element={(<></>)}/>
-            <Route path='/new-post' element={(<></>)}/>
+            <Route path='/' element={(<HomePage/>)}/>
+            <Route path='/:id' element={(<PostPage/>)}/>
+            <Route path='/new-post' element={(<PostForm/>)}/>
             <Route path='/about' element={(<></>)}/>
             <Route path='contacts' element={(<></>)}/>
         </Routes>
---
+++ src/types.d.ts
@@ -0,0 +1,5 @@
+export interface Post {
+    message:string;
+    date:string;
+    id?:string;
+}
